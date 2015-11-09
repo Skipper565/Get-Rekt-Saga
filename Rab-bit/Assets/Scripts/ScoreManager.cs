aad2cd4c919a3d83822c6e714298ef0107f34ccd
Kristýna Leknerová <leknerova@gmail.com>
2015-11-09 13:03:33 +0100

    Score counter
    Based on camera position. To work in levels other than MouthScene,
    GameManager will be required (+ camera position at the end of each
    completed level).

---
+++ Rab-bit/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text scoreText;
+    public float distanceModifier;
+
+    private float distance = 0;
+
+	// Use this for initialization
+	void Start () {
+        scoreText.text = "0";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distance = Camera.main.transform.position[0] * distanceModifier;
+        scoreText.text = "" + (int)distance;
+	}
+}
