3e36bb7ade186c252be35319253ff5e90b7e68d5
Lukas Lizal <luky.lizal@gmail.com>
2016-01-25 23:32:56 +0100

    Water animation changes
    - water animation was changed to only horizontal movement
    - changed scale of Top (wooden bar) to prefabs better follow each other

--- Rab-bit/Assets/Scripts/SeawaveMovement.cs
+++ Rab-bit/Assets/Scripts/SeawaveMovement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class SeawaveMovement : MonoBehaviour
@@ -6,16 +6,13 @@ public class SeawaveMovement : MonoBehaviour
     public float amplitude;
     public float frequency;
     public float angle;
-    public float height;
     
 	
 	// Update is called once per frame
 	void Update () {
         angle += frequency * Time.deltaTime;
-        if (angle > 360) angle -= 360;
-
 	    var newPosition = transform.localPosition;
-        newPosition.y = amplitude * Mathf.Sin(angle * (Mathf.PI / 180)) + height;
-	    transform.localPosition = newPosition;
+        newPosition.x = amplitude * Mathf.Sin(angle * (Mathf.PI / 180))*0.1f;
+		transform.localPosition = new Vector3(transform.localPosition.x+newPosition.x,transform.localPosition.y, transform.localPosition.z);
 	}
 }
