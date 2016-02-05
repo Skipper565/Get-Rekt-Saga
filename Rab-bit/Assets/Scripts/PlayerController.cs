b785f3d7fe8e9c5275a573d6d4c26a1fef527073
Kristýna Leknerová <leknerova@gmail.com>
2016-02-05 17:52:07 +0100

    Dive in space pavilon workaround
    So that we don't dive into the void

--- Rab-bit/Assets/Scripts/PlayerController.cs
+++ Rab-bit/Assets/Scripts/PlayerController.cs
@@ -452,14 +452,20 @@ public class PlayerController : MonoBehaviour {
         var distanceToCollider = Physics2D.Raycast(transform.position, -transform.up).distance;
         var targetCollider = Physics2D.Raycast(transform.position, -transform.up).collider;
 
+        if (targetCollider == null)
+        {
+            //Debug.Log("DISTANCE TO COLLIDER: " + distanceToCollider);
+            return;
+        }
+
         jumpSounds[UnityEngine.Random.Range(0, 4)].Play();
 
         // If distance to collider is smaller than our usual dive, teleport to the collider, no further.
         if (distanceToCollider <= diveLength && !targetCollider.tag.Contains("PowerUp"))
         {
             // Uncomment this for determining distance of the player and the collider. Set value collisionTolerance accordingly.
-            Debug.Log("Move down: Collider: " + targetCollider.name + " Distance: " + distanceToCollider 
-                + " In the gravity direction: " + (rb.gravityScale > 0 ? "Down" : "Up"));
+            /*Debug.Log("Move down: Collider: " + targetCollider.name + " Distance: " + distanceToCollider 
+                + " In the gravity direction: " + (rb.gravityScale > 0 ? "Down" : "Up"));*/
 
             if (distanceToCollider <= collisionTolerance)
             {
